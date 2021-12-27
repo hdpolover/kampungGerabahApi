45cff06097ee78f353a0484a0c5449b733a6f801
hdpolover <hendrapolover@gmail.com>
2021-12-27 22:29:08 +0700

    up

--- application/controllers/api/Pengguna.php
+++ application/controllers/api/Pengguna.php
@@ -2,6 +2,7 @@
 defined('BASEPATH') or exit('No direct script access allowed');
 
 use chriskacerguis\RestServer\RestController;
+use phpDocumentor\Reflection\Types\Self_;
 
 class Pengguna extends RestController
 {
