94a6e21d160575030f0c7e0fd7178ae2e4e4f55c
hdpolover <hendrapolover@gmail.com>
2022-01-16 22:28:35 +0700

    up

--- kampung_gerabah_db.sql
+++ kampung_gerabah_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 26, 2021 at 04:54 PM
+-- Generation Time: Jan 16, 2022 at 04:17 PM
 -- Server version: 10.4.20-MariaDB
 -- PHP Version: 7.3.29
 
@@ -24,6 +24,49 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `detail_transaksi`
+--
+
+CREATE TABLE `detail_transaksi` (
+  `id_detail_transaksi` int(11) NOT NULL,
+  `id_transaksi` varchar(20) NOT NULL,
+  `id_produk` int(11) NOT NULL,
+  `jumlah` int(11) NOT NULL,
+  `subtotal` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `keranjang`
+--
+
+CREATE TABLE `keranjang` (
+  `id_keranjang` int(11) NOT NULL,
+  `id_pengguna` int(11) NOT NULL,
+  `id_produk` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pembayaran`
+--
+
+CREATE TABLE `pembayaran` (
+  `id_pembayaran` int(11) NOT NULL,
+  `id_transaksi` varchar(20) NOT NULL,
+  `bukti` varchar(100) NOT NULL,
+  `bank_pengirim` varchar(30) NOT NULL,
+  `nama_akun_pengirim` varchar(30) NOT NULL,
+  `status` int(11) NOT NULL COMMENT '0: menunggu validasi, 1: valid',
+  `tgl_transfer` date NOT NULL,
+  `nominal_transfer` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `pengguna`
 --
 
@@ -36,12 +79,85 @@ CREATE TABLE `pengguna` (
   `foto` varchar(100) NOT NULL,
   `alamat` varchar(100) NOT NULL,
   `tgl_daftar` date NOT NULL,
-  `status` int(11) NOT NULL
+  `status` int(11) NOT NULL,
+  `no_telp` varchar(20) NOT NULL,
+  `latitude` varchar(30) NOT NULL,
+  `longitude` varchar(30) NOT NULL,
+  `id_kota` int(11) NOT NULL,
+  `nama_kota` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `pengguna`
+--
+
+INSERT INTO `pengguna` (`id_pengguna`, `peran`, `email`, `password`, `username`, `foto`, `alamat`, `tgl_daftar`, `status`, `no_telp`, `latitude`, `longitude`, `id_kota`, `nama_kota`) VALUES
+(2, 'customer', 'ali@email.com', '123456', 'nurali', 'default.jpg', 'Jl. Dieng Atas No.9, Sumberjo, Kalisongo, Kec. Dau, Kabupaten Malang, Jawa Timur 65151', '2021-12-27', 1, '23232', '-7.973006', '112.6079458', 255, 'Kabupaten Malang'),
+(7, 'admin', 'admin@email.com', 'admin', 'admin', 'default.jpg', '', '2022-01-19', 1, '2212121', '', '', 0, ''),
+(8, 'pengrajin', 'p@email.com', 'pengrajin', 'pengrajin', 'default.jpg', 'Jl. Dieng Atas No.9, Sumberjo, Kalisongo, Kec. Dau, Kabupaten Malang, Jawa Timur 65151', '2022-01-05', 1, '12123123', '-7.973006', '112.6079458', 0, '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `produk`
+--
+
+CREATE TABLE `produk` (
+  `id_produk` int(11) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `deskripsi` varchar(200) NOT NULL,
+  `stok` int(11) NOT NULL,
+  `id_pengguna` int(11) NOT NULL,
+  `harga_satuan` int(11) NOT NULL,
+  `gambar` varchar(100) NOT NULL,
+  `berat` int(11) NOT NULL,
+  `kategori` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaksi`
+--
+
+CREATE TABLE `transaksi` (
+  `id_transaksi` varchar(20) NOT NULL,
+  `id_pengguna` int(11) NOT NULL,
+  `total` int(11) NOT NULL,
+  `tgl_transaksi` datetime NOT NULL,
+  `no_resi` varchar(30) NOT NULL,
+  `kurir` varchar(30) NOT NULL,
+  `status_transaksi` int(11) NOT NULL,
+  `status_pengiriman` int(11) NOT NULL,
+  `ongkir` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `detail_transaksi`
+--
+ALTER TABLE `detail_transaksi`
+  ADD PRIMARY KEY (`id_detail_transaksi`),
+  ADD KEY `vvzxv` (`id_transaksi`),
+  ADD KEY `vzvw` (`id_produk`);
+
+--
+-- Indexes for table `keranjang`
+--
+ALTER TABLE `keranjang`
+  ADD PRIMARY KEY (`id_keranjang`),
+  ADD KEY `fk_id_pengrws` (`id_pengguna`),
+  ADD KEY `sdfdf` (`id_produk`);
+
+--
+-- Indexes for table `pembayaran`
+--
+ALTER TABLE `pembayaran`
+  ADD PRIMARY KEY (`id_pembayaran`),
+  ADD KEY `vxvzv` (`id_transaksi`);
 
 --
 -- Indexes for table `pengguna`
@@ -50,14 +166,88 @@ ALTER TABLE `pengguna`
   ADD PRIMARY KEY (`id_pengguna`);
 
 --
+-- Indexes for table `produk`
+--
+ALTER TABLE `produk`
+  ADD PRIMARY KEY (`id_produk`),
+  ADD KEY `fk_id_pengguna` (`id_pengguna`);
+
+--
+-- Indexes for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD PRIMARY KEY (`id_transaksi`),
+  ADD KEY `sfafsaf` (`id_pengguna`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `detail_transaksi`
+--
+ALTER TABLE `detail_transaksi`
+  MODIFY `id_detail_transaksi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `keranjang`
+--
+ALTER TABLE `keranjang`
+  MODIFY `id_keranjang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `pembayaran`
+--
+ALTER TABLE `pembayaran`
+  MODIFY `id_pembayaran` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `pengguna`
 --
 ALTER TABLE `pengguna`
-  MODIFY `id_pengguna` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_pengguna` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `produk`
+--
+ALTER TABLE `produk`
+  MODIFY `id_produk` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `detail_transaksi`
+--
+ALTER TABLE `detail_transaksi`
+  ADD CONSTRAINT `vvzxv` FOREIGN KEY (`id_transaksi`) REFERENCES `transaksi` (`id_transaksi`),
+  ADD CONSTRAINT `vzvw` FOREIGN KEY (`id_produk`) REFERENCES `produk` (`id_produk`);
+
+--
+-- Constraints for table `keranjang`
+--
+ALTER TABLE `keranjang`
+  ADD CONSTRAINT `fk_id_pengrws` FOREIGN KEY (`id_pengguna`) REFERENCES `pengguna` (`id_pengguna`),
+  ADD CONSTRAINT `sdfdf` FOREIGN KEY (`id_produk`) REFERENCES `produk` (`id_produk`);
+
+--
+-- Constraints for table `pembayaran`
+--
+ALTER TABLE `pembayaran`
+  ADD CONSTRAINT `vxvzv` FOREIGN KEY (`id_transaksi`) REFERENCES `transaksi` (`id_transaksi`);
+
+--
+-- Constraints for table `produk`
+--
+ALTER TABLE `produk`
+  ADD CONSTRAINT `fk_id_pengguna` FOREIGN KEY (`id_pengguna`) REFERENCES `pengguna` (`id_pengguna`);
+
+--
+-- Constraints for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD CONSTRAINT `sfafsaf` FOREIGN KEY (`id_pengguna`) REFERENCES `pengguna` (`id_pengguna`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
